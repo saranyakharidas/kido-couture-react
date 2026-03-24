933557204f45ec220cf04c74c9396e1335f66710
saranyakharidas <saranyakh307@gmail.com>
2026-03-24 12:38:26 +0530

    Fixed Vercel build output path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     manifest: true,
-    outDir: '../static/dist',
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: './src/main.jsx',
